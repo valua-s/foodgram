6776006d8048be315314d0632bfefc87c3dbcf01
Valua-s <speranskaya.valya@yandex.ru>
2024-08-07 20:29:37 +0800

    after tests

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,6 @@ from api.views import redirect_link
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
