5d4edf560b9108c15776ac0ce3a65f1db57e403c
Valua-s <speranskaya.valya@yandex.ru>
2024-08-09 11:24:10 +0800

    after first review

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import (MaxValueValidator,
-                                    MinValueValidator,
+from django.core.validators import (MaxValueValidator, MinValueValidator,
                                     RegexValidator)
 from django.db import models
 from django.utils.text import slugify
