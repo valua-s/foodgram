22ed6246195b4ade7e7aa96a3fef04c000815c93
Valua-s <speranskaya.valya@yandex.ru>
2024-08-21 13:25:09 +0800

    changes in load_ing

--- backend/reviews/management/commands/load_ingredients.py
+++ backend/reviews/management/commands/load_ingredients.py
@@ -9,19 +9,11 @@ class Command(BaseCommand):
     help = 'Load ingredients from CSV file'
 
     def handle(self, *args, **kwargs):
-        file_path = os.path.join('app', 'data', 'ingredients.csv')
+        file_path = os.path.join('data', 'ingredients.csv')
         with open(file_path, mode='r', encoding='utf-8') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                ingredient, created = Ingredient.objects.get_or_create(
+                ingredient = Ingredient.objects.create(
                     name=row['name'],
                     measurement_unit=row['measurement_unit']
                 )
-                if created:
-                    self.stdout.write(self.style.SUCCESS(
-                        f'Ingredient "{ingredient.name}" added.'
-                    ))
-                else:
-                    self.stdout.write(self.style.WARNING(
-                        f'Ingredient "{ingredient.name}" already exists.'
-                    ))
