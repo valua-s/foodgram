ded8bd687771d5e3e9e4973e9608d08085420eb0
Valua-s <speranskaya.valya@yandex.ru>
2024-08-26 17:44:20 +0800

    changes pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,5 +1,5 @@
-from rest_framework.pagination import (PageNumberPagination,
-                                       LimitOffsetPagination)
+from rest_framework.pagination import (LimitOffsetPagination,
+                                       PageNumberPagination)
 
 
 class LimitOffsetPaginationRecipesParam(PageNumberPagination):
