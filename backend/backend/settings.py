b9c71ac3694efa3b1cafbae67a661b37479c0480
Valua-s <speranskaya.valya@yandex.ru>
2024-09-13 10:47:56 +0800

    trying to up DB

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,10 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import environ
 from pathlib import Path
 
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+
+env = environ.Env()
+env.read_env(env.str('ENV_PATH', '/path/to/.env'))
 
 sentry_sdk.init(
     dsn="https://9fbaf6cf9ecc08f712b414ca4cffc620@o4507349538897920.ingest.us.sentry.io/4507938923544576",
@@ -32,10 +36,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['84.252.139.254', '127.0.0.1', 'localhost', 'foodgram-valuas.ddns.net']
 
@@ -102,11 +106,11 @@ AUTH_USER_MODEL = 'reviews.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT')
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT')
     }
 }
 
