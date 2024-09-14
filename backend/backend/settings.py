4f6499bcb91c217fac175fe9f3edac3a7d6c1522
Valua-s <speranskaya.valya@yandex.ru>
2024-09-14 10:29:25 +0800

    try

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', ''),
+        'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
